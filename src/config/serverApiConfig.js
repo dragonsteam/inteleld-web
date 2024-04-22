cef079d813feeb8ca6405c98b49710dc6f9edb76
Nick <nickphilomath@gmail.com>
2024-04-22 04:45:27 -0400

    update config file

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,9 +1,12 @@
-export const API_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? 'api/'
-    : 'http://localhost:8000/api/';
-export const BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE ? '' : 'http://localhost:8000/';
+export const BASE_URL = import.meta.env.MODE === 'development' ? 'http://127.0.0.1:8000/' : '';
+export const API_BASE_URL = BASE_URL + 'api/';
+
+// export const API_BASE_URL =
+//   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
+//     ? 'api/'
+//     : 'http://localhost:8000/api/';
+// export const BASE_URL =
+//   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE ? '' : 'http://localhost:8000/';
 
 // export const WEBSITE_URL = import.meta.env.PROD
 //   ? 'http://cloud.idurarapp.com/'
