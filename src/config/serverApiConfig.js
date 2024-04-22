291543fc77e628bde2bf6567ba78507b673f9161
Nick <nickphilomath@gmail.com>
2024-04-22 04:38:41 -0400

    update config file

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,19 +1,17 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
+    ? 'api/'
     : 'http://localhost:8000/api/';
 export const BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8000/';
+  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE ? '' : 'http://localhost:8000/';
 
-export const WEBSITE_URL = import.meta.env.PROD
-  ? 'http://cloud.idurarapp.com/'
-  : 'http://localhost:3000/';
-export const DOWNLOAD_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8000/download/';
-export const ACCESS_TOKEN_NAME = 'x-auth-token';
+// export const WEBSITE_URL = import.meta.env.PROD
+//   ? 'http://cloud.idurarapp.com/'
+//   : 'http://localhost:3000/';
+// export const DOWNLOAD_BASE_URL =
+//   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
+//     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
+//     : 'http://localhost:8000/download/';
+// export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
-export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
+// export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
