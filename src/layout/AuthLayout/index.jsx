2d2860362dadab3b5a01ff80491ec7dcee1cd130
Nick <nickphilomath@gmail.com>
2024-04-22 04:29:38 -0400

    now this is proper merge

---
+++ src/layout/AuthLayout/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+export default function AuthLayout({ sideContent, children }) {
+  return (
+    <Layout style={{ textAlign: 'left' }}>
+      {/* <Content
+        style={{
+          padding: '10px 20px',
+        }}
+      >
+        <div>select language</div>
+      </Content> */}
+      <Row>
+        <Col
+          xs={{ span: 0, order: 2 }}
+          sm={{ span: 0, order: 2 }}
+          md={{ span: 11, order: 1 }}
+          lg={{ span: 12, order: 1 }}
+          style={{
+            minHeight: '100vh',
+          }}
+        >
+          {sideContent}
+        </Col>
+        <Col
+          xs={{ span: 24, order: 1 }}
+          sm={{ span: 24, order: 1 }}
+          md={{ span: 13, order: 2 }}
+          lg={{ span: 12, order: 2 }}
+          style={{ background: '#FFF', minHeight: '100vh' }}
+        >
+          {children}
+        </Col>
+      </Row>
+    </Layout>
+  );
+}
