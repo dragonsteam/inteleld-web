3f4e473939cb1002eed728164ba374a09af66dde
Nick <nickphilomath@gmail.com>
2024-05-13 21:54:25 +0500

    Dockerize the project for developement

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    base: "/",
+    base: '/',
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
+    host: true,
     port: 3000,
+    watch: {
+      usePolling: true,
+    },
   },
 });
