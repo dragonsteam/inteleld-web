59f1128f2f7f3c44a672e6042f54a57af708760b
Nick <nickphilomath@gmail.com>
2024-05-22 20:10:57 +0500

    fix close editbox when panel close

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -5,7 +5,7 @@ import { useCrudContext } from '@/context/crud';
 export default function SidePanel({ config, topContent, bottomContent }) {
   const { state, crudContextAction } = useCrudContext();
   const { isPanelClose } = state;
-  const { panel } = crudContextAction;
+  const { panel, editBox } = crudContextAction;
 
   const { PANEL_TITLE } = config;
 
@@ -15,6 +15,7 @@ export default function SidePanel({ config, topContent, bottomContent }) {
       placement="right"
       onClose={() => {
         panel.close();
+        editBox.close();
       }}
       open={!isPanelClose}
       width={450}
