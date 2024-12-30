145a703851781fcd247a0182b733fde411038b69
Nick <nickphilomath@gmail.com>
2024-12-30 20:06:44 +0000

    update tollreports

--- src/pages/TollReports/config.js
+++ src/pages/TollReports/config.js
@@ -1,3 +1,9 @@
+export const REPORT_STATUSES = [
+  { value: 1, label: 'Waiting for process', color: 'magenta' },
+  { value: 2, label: 'Report is being processed', color: 'purple' },
+  { value: 3, label: 'Processed', color: 'green' },
+];
+
 export const fields = {
   report_file: {
     type: 'file',
@@ -7,6 +13,13 @@ export const fields = {
   notes: {
     type: 'textarea',
     label: 'Notes',
+  },
+  status: {
+    type: 'select',
+    label: 'Status',
+    options: REPORT_STATUSES,
+    read_only: true,
+    renderAsTag: true,
   },
   created_at: {
     type: 'datetime',
